d510871683f9ad7a56ed20e6e0fcde53b7954b4f
xiaomingnevermind <sunming.5211@163.com>
2019-10-10 14:02:04 +0800

    添加 sched lock ；解决问题：基于springboot 自身得框架启动多个定时任务，不能够同时跑多个任务得问题修复。

--- src/main/java/com/example/demo/job/TestJob01.java
+++ src/main/java/com/example/demo/job/TestJob01.java
@@ -1,11 +1,8 @@
 package com.example.demo.job;
 
 import net.javacrumbs.shedlock.core.SchedulerLock;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -14,19 +11,12 @@ import java.time.LocalDateTime;
 @EnableScheduling
 public class TestJob01 {
 
-    @Bean
-    public TaskScheduler taskScheduler() {
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.initialize();
-        taskScheduler.setPoolSize(50);
-        return taskScheduler;
-    }
 
     @Scheduled(cron = "0/5 * * * * ?")
-    @SchedulerLock(name = "test4-task",lockAtMostForString = "PT28M",lockAtLeastForString = "PT28M")
-    public void execute1(){
-        String curName = Thread.currentThread().getName() ;
-        System.out.println("当前时间:"+LocalDateTime.now()+"  任务execute4对应的线程名: "+curName);
+    @SchedulerLock(name = "test4-task", lockAtMostForString = "PT28M", lockAtLeastForString = "PT28M")
+    public void execute1() {
+        String curName = Thread.currentThread().getName();
+        System.out.println("当前时间:" + LocalDateTime.now() + "  任务execute4对应的线程名: " + curName);
         try {
             Thread.sleep(1000);
         } catch (Exception e) {
@@ -36,11 +26,11 @@ public class TestJob01 {
     }
 
     @Scheduled(cron = "0/5 * * * * ?")
-    @SchedulerLock(name = "test5-task",lockAtMostForString = "PT28M",lockAtLeastForString = "PT28M")
-    public void execute2(){
+    @SchedulerLock(name = "test5-task", lockAtMostForString = "PT28M", lockAtLeastForString = "PT28M")
+    public void execute2() {
 
-        String curName = Thread.currentThread().getName() ;
-        System.out.println("当前时间:"+ LocalDateTime.now()+"  任务execute5对应的线程名: "+curName);
+        String curName = Thread.currentThread().getName();
+        System.out.println("当前时间:" + LocalDateTime.now() + "  任务execute5对应的线程名: " + curName);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -49,11 +39,11 @@ public class TestJob01 {
     }
 
     @Scheduled(cron = "0/10 * * * * ?")
-    @SchedulerLock(name = "test6-task",lockAtMostForString = "PT28M",lockAtLeastForString = "PT28M")
-    public void execute3(){
+    @SchedulerLock(name = "test6-task", lockAtMostForString = "PT28M", lockAtLeastForString = "PT28M")
+    public void execute3() {
 
-        String curName = Thread.currentThread().getName() ;
-        System.out.println("当前时间:"+ LocalDateTime.now()+"  任务execute6对应的线程名: "+curName);
+        String curName = Thread.currentThread().getName();
+        System.out.println("当前时间:" + LocalDateTime.now() + "  任务execute6对应的线程名: " + curName);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
