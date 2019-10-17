9de315942cf79eb544752669a8f0375d2b4d896e
xiaomingnevermind <sunming.5211@163.com>
2019-10-17 09:53:00 +0800

    添加 sched lock 在分布式环境下对定时任务加锁，避免有定时任务在多台服务器下重复执行 ；解决问题：基于springboot 自身得框架启动多个定时任务，不能够同时跑多个任务得问题修复。

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
