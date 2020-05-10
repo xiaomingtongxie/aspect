bae180ab577da9ce7668abf9b37656a29f162720
xmtx <xmtx.2015@gmail.com>
2020-05-10 16:11:24 +0800

    # 线程状态流转演示

---
+++ src/main/java/com/example/demo/thread/ThreadBlocked.java
@@ -0,0 +1,32 @@
+package com.example.demo.thread;
+
+public class ThreadBlocked extends Thread {
+
+    private byte[] lock = new byte[0];
+
+    public ThreadBlocked(byte[] lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (lock) {
+            try {
+                Thread.sleep(10000);// 休眠十秒，防止过快结束
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        byte[] lock = new byte[0];
+        ThreadNewRunable threadNewRunable = new ThreadNewRunable(lock);
+        threadNewRunable.start();
+        ThreadNewRunable threadNewRunable1 = new ThreadNewRunable(lock);
+        threadNewRunable1.start();
+        Thread.sleep(1000);
+        System.out.println("threadStatu1-blocked : " + threadNewRunable1.getState());
+    }
+}
