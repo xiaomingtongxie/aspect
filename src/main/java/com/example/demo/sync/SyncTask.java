a922eeff7af4af6d0aa35f4c15e83260e7a5b884
xmtx <xmtx.2015@gmail.com>
2021-01-29 22:14:01 +0800

    # 最新个税公示实现

--- src/main/java/com/example/demo/sync/SyncTask.java
+++ src/main/java/com/example/demo/sync/SyncTask.java
@@ -15,14 +15,14 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping(value = "/task")
 public class SyncTask {
     @PostMapping(value = "/sync")
-    public String postAspect(@RequestBody AspectBean aspectBean) throws InterruptedException {
+    public String testSync() throws InterruptedException {
 
         // 开启异步任务执行任务
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() ->
                 {
                     try {
+                        System.out.println("我进到异步方法里面了=======================");
                         this.sync();
-
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -30,16 +30,19 @@ public class SyncTask {
                 }
         );
         try {
+            // 必须在future.complete之前sleep 异步线程才能执行
+            Thread.sleep(200);
             future.complete("执行成功！");
-            // 这里直接返回前端结果
+            // 这里直接返回前端结果 ,这里sleep无效
+            Thread.sleep(2000);
             return future.get();
         } catch (ExecutionException e) {
             e.printStackTrace();
         } finally {
             // do nothing
         }
-        // 这里主线程不休眠的话，异步线程池会秒结束，这个不知道什么机制，需要研究一下
-        Thread.sleep(20);
+        // 这里主线程不休眠的话，异步线程池会秒结束，这个不知道什么机制，需要研究一下，这里sleep无效
+        Thread.sleep(2000);
         return "方法结束";
     }
 
@@ -52,7 +55,8 @@ public class SyncTask {
      */
     private String sync() throws InterruptedException {
 
-        Thread.sleep(1000 * 60 * 10);
+        System.out.println("执行到耗时方法了！");
+        Thread.sleep(1000 * 10);
         return "success";
     }
 
